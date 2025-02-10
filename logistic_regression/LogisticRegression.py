e8b92f9a66d4b918dfa2f9ca1e512245657fcf29
sanoj c sam <sanojcsam123@gmail.com>
2025-02-10 18:45:42 +0530

    all file commited

---
+++ logistic_regression/LogisticRegression.py
@@ -0,0 +1,56 @@
+
+import numpy as np
+
+
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+class LogisticRegression():
+
+    def __init__(self,lr=0.001 ,n_iterations=1000):
+
+        self.lr=lr
+        self.n_iterations=n_iterations
+        self.weight= None
+        self.bias =None
+
+      
+
+    def fit(self, X,y):
+
+        n_samples, n_features = X.shape
+        self.weights =np.zeros(n_features) 
+        self.bias = 0
+
+        ##predict result
+
+        for _ in range(self.n_iterations):
+            ## wx+ b
+            linear_pred = np.dot(X,self.weights) + self.bias
+
+            predictions =  sigmoid(linear_pred)
+
+            #weigth     calculate error
+            dw = (1/n_samples) * np.dot(X.T,(predictions - y) )
+
+
+            ##bias
+
+            db = (1/n_samples) * np.sum(predictions - y)
+
+            self.weights = self.weights - self.lr * dw
+
+            self.bias = self.bias - self.lr * db
+
+    def predict(self,X):
+
+        linear_pred = np.dot(X,self.weights) + self.bias
+        y_pred = sigmoid(linear_pred)
+
+
+        class_pred= [0 if y<=0.5 else 1 for y in y_pred]
+        
+        return class_pred
